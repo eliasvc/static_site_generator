4f4ee0c8d609facc99f509d45744265392341496
Elias <elias.vc@gmail.com>
2024-03-18 14:40:23 -0400

    first commit

---
+++ src/main.py
@@ -0,0 +1,12 @@
+from textnode import TextNode
+
+def main():
+    bold_text = TextNode("boom", "bold", "example.com")
+    more_bold_text = TextNode("boom", "bold", "example.com")
+    italic_text = TextNode("boom", "italic", "example.com")
+    print(bold_text)
+    print(bold_text == more_bold_text)
+    print(bold_text == italic_text)
+
+if __name__ == "__main__":
+    main()
---
+++ src/test_textnode.py
@@ -0,0 +1,34 @@
+import unittest
+
+from textnode import TextNode
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", "bold")
+        node2 = TextNode("This is a text node", "bold")
+        self.assertEqual(node, node2)
+    
+    def test_eq_url(self):
+        node = TextNode("This is a text node", "bold", "example.com")
+        node2 = TextNode("This is a text node", "bold", "example.com")
+        self.assertEqual(node, node2)
+
+    def test_eq_false_type(self):
+        node = TextNode("This is a text node", "bold")
+        node2 = TextNode("This is a text node", "italic")
+        self.assertNotEqual(node, node2)
+
+    def test_eq_false_text(self):
+        node = TextNode("This is a text node", "italic")
+        node2 = TextNode("This might be a text node", "bold")
+        self.assertNotEqual(node, node2)
+
+    def test_eq_false_url(self):
+        node = TextNode("This is a text node", "bold", "example.com")
+        node2 = TextNode("This is a text node", "bold")
+        self.assertNotEqual(node, node2)
+
+if __name__ == "__main__":
+    unittest.main()
+
