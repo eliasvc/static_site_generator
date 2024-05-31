863d1250ca80e9f3fb4047a9ddeb305f41397abe
Elias Valdez <evaldez@gmail.com>
2024-05-31 11:22:12 -0400

    Shorten test text and add empty image markdown test

--- src/test_parse.py
+++ src/test_parse.py
@@ -5,16 +5,18 @@ import parse
 
 class TestParse(unittest.TestCase):
     def test_extract_markdown_image(self):
-        text = "This is text with an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and ![another](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/dfsdkjfd.png)"
+        text = "This is text with an ![image](https://test.com/boom/boom.png) and ![another](https://www.test.com/kaboom/kabOOM233.jpeg)"
         expected_output = [
-            (
-                "image",
-                "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png",
-            ),
-            (
-                "another",
-                "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/dfsdkjfd.png",
-            ),
+            ("image", "https://test.com/boom/boom.png"),
+            ("another", "https://www.test.com/kaboom/kabOOM233.jpeg"),
+        ]
+        output = parse.extract_markdown_images(text)
+        self.assertEqual(output, expected_output)
+
+    def test_extract_markdown_image_empty_image(self):
+        text = "This is text with an ![]()"
+        expected_output = [
+            ("", ""),
         ]
         output = parse.extract_markdown_images(text)
         self.assertEqual(output, expected_output)
